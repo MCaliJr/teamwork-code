0fb527b15bb7656c3ef2a22c5b98cf51f5d97185
MCaliJr <martink.moc@gmail.com>
2023-11-16 15:14:09 +0100

    read csv file tests and implementation

--- interview.go
+++ interview.go
@@ -4,3 +4,49 @@
 // logged (or handled). Performance matters (this is only ~3k lines, but *could*
 // be 1m lines or run on a small machine).
 package customerimporter
+
+import (
+	"bufio"
+	"encoding/csv"
+	"os"
+	"sync"
+	"strings"
+)
+
+// read data from a CSV file using concurrency for faster read time
+func readCSV(filename string) ([][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+			return nil, err
+	}
+	defer file.Close()
+
+	var records [][]string
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+			wg.Add(1)
+			go func(data string) {
+					defer wg.Done()
+					reader := csv.NewReader(strings.NewReader(data))
+					record, err := reader.Read()
+					if err != nil {
+							// TODO handle / log error
+							return
+					}
+
+					mutex.Lock()
+					records = append(records, record)
+					mutex.Unlock()
+			}(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+			return nil, err
+	}
+
+	wg.Wait()
+	return records, nil
+}
--- interview_test.go
+++ interview_test.go
@@ -1,1 +1,38 @@
 package customerimporter
+
+import (
+    "reflect"
+    "testing"
+		"strings"
+)
+
+func TestReadCSV(t *testing.T) {
+    filename := "test_customers.csv"
+    expected := [][]string{
+        {"first_name", "last_name", "email", "gender", "ip_address"},
+        {"John", "Doe", "thatGuyDoe@faceSmile.net", "Male", "53.191.87.821"},
+        {"Mildred", "Hernandez", "mhernandez0@github.io", "Female", "38.194.51.128"},
+        {"Bonnie", "Ortiz", "bortiz1@cyberchimps.com", "Female", "197.54.209.129"},
+        {"Dennis", "Henry", "dhenry2@hubpages.com", "Male", "155.75.186.217"},
+    }
+
+    records, err := readCSV(filename)
+    if err != nil {
+        t.Errorf("readCSV returned an error: %v", err)
+    }
+
+		// Convert records to maps for comparison with ignored order
+		expectedMap := make(map[string]struct{})
+		for _, record := range expected {
+				expectedMap[strings.Join(record, ",")] = struct{}{}
+		}
+
+		recordsMap := make(map[string]struct{})
+		for _, record := range records {
+				recordsMap[strings.Join(record, ",")] = struct{}{}
+		}
+
+		if !reflect.DeepEqual(expectedMap, recordsMap) {
+				t.Errorf("readCSV did not return the expected records")
+		}
+}
